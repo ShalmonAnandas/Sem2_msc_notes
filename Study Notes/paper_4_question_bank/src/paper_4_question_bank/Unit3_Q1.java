861e3b299574b5c60b878e28bb04cc02795f4884
ShalmonAnandas <nashalanandas@gmail.com>
2022-06-19 22:38:35 +0530

    Signed-off-by: ShalmonAnandas <nashalanandas@gmail.com>

--- Study Notes/paper_4_question_bank/src/paper_4_question_bank/Unit3_Q1.java
+++ Study Notes/paper_4_question_bank/src/paper_4_question_bank/Unit3_Q1.java
@@ -1,49 +1,41 @@
-package paper_4_question_bank;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package paper_4_question_bank;
 
 /**
  *
  * @author Shalmon
  */
-
+import java.sql.*;
 import javax.swing.*;
-
-public class Unit2_Q13{
+public class Unit3_Q1 {
     
-    public Unit2_Q13(){
+    public Unit3_Q1(){
         //creating frame
         JFrame f = new JFrame();
         
         //creating elements
-        JLabel label1 = new JLabel("Enter the first no: ");
+        JLabel label1 = new JLabel("User name: ");
         label1.setBounds(50,50,100,20);
         JTextField tf1 = new JTextField();
         tf1.setBounds(200,50,100,20);
-        JLabel label2 = new JLabel("Enter the Second no: ");
+        JLabel label2 = new JLabel("Password: ");
         label2.setBounds(50,100,150,20);
         JTextField tf2 = new JTextField();
         tf2.setBounds(200,100,100,20);
-        JLabel label3 = new JLabel("The sum is: ");
-        label3.setBounds(50,150,150,20);
-        JLabel label4 = new JLabel("");
-        label4.setBounds(200,150,150,20);
         JButton b1 = new JButton("ADD");
         b1.setBounds(50, 200, 125, 20);
         JButton b2 = new JButton("CLEAR");
         b2.setBounds(200, 200, 125, 20);
         
-        //adding elemts to the frame
+        //adding elements to the frame
         f.add(label1);
         f.add(tf1);
         f.add(label2);
         f.add(tf2);
-        f.add(label3);
-        f.add(label4);
         f.add(b1);
         f.add(b2);
         
@@ -53,9 +45,20 @@ public class Unit2_Q13{
         f.setVisible(true);
     }
     
-    
-    
     public static void main(String[] args){
-        Unit2_Q13 u = new Unit2_Q13();
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con;
+            Statement smt;
+            con = DriverManager.getConnection("jdbc:mysql://localhost/world", "root", "root");
+            smt=con.createStatement();
+            
+            
+            
+        }catch(SQLException se){
+            se.printStackTrace();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
